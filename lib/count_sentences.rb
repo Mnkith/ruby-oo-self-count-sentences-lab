09ee996df2d4d970d8a8b399cf83ecb284438f98
Mnkith <munkithabid@gmail.com>
2020-11-06 02:03:04 -0600

    commit tree e9059e8fd9865af98d9f04bb3e2d449d7ec7b7eb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
+    # self[self.length - 1] == '.'
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    # self.split(/[?!.]+/).length      #simple showcase of regex beauty
+    sentences_array = split('!').join('.').split('?').join('.').split('.') # Oh well...!
+    count = 0
+    sentences_array.each{ | sentence | count += 1 if sentence != ''}
+    count
   end
 end
